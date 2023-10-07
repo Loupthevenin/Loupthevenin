13c4ca134c0c050dbda4a8324a59348c07546dec
MrShadowsWolf <loup.thevenin.1@gmail.com>
2023-10-07 17:35:34 +0200

    Patch 2

--- main.py
+++ main.py
@@ -5,7 +5,9 @@ import random
 path_json = "data_quote.json"
 path_history = "data_history.json"
 path_readme = "README.md"
-base_history = {"history_choices": []}
+base_history = {"history_movie_choices": [],
+                "history_quote_choices": []
+                }
 tag_quote_start = "<!-- INSERT QUOTE START -->"
 tag_quote_end = "<!-- INSERT QUOTE END -->"
 
@@ -17,10 +19,18 @@ with open(path_json, "r", encoding="utf-8") as f:
 with open(path_history, "r", encoding="utf-8") as f:
     history = json.load(f)
 
-random_choice = random.randint(0, len(db["quotes"]))
+# On tire au sort parmi les films dans un premier temps
+ids_movie = list(set(db["ids"]))
+random_movie_choice = random.randint(0, len(ids_movie))
+id_movie = ids_movie[random_movie_choice]
+index_id = [i for i, valeur in enumerate(db["ids"]) if valeur == id_movie]
 
-random_quote = db["quotes"][random_choice]
-random_character = db["character"][random_choice]
+# Puis, on tire au sort la citation du film
+random_quote_choice = random.randint(index_id[0], index_id[-1])
+
+# Resultat
+random_quote = db["quotes"][random_quote_choice]
+random_character = db["character"][random_quote_choice]
 
 
 if "\n" in random_quote:
@@ -30,9 +40,9 @@ if "\n" in random_quote:
 # verifie que le json a le meme modèle que le script
 if base_history.keys() != history.keys():
     history = base_history
-history["history_choices"].append(random_choice)
+history["history_movie_choices"].append(id_movie)
+history["history_quote_choices"].append(random_quote_choice)
 
-total_history_quote = len(history["history_choices"])
 
 # ajout du random choice actuel
 with open(path_history, "w", encoding="utf-8") as f:
@@ -41,6 +51,16 @@ with open(path_history, "w", encoding="utf-8") as f:
 # load le readme
 with open(path_readme, "r") as f:
     read = f.read()
+
+
+# Pour le bot plus tard
+try:
+    chosen = len(history["history_movie_choices"][id_movie])
+except IndexError:
+    chosen = 0
+total_movies = len(ids_movie)
+WolfyBOT = f"🤖 WolfyBOT 🤖 vous informe que ce film a été tiré **{chosen}** fois au sort parmis les **{total_movies}**. 🎲🎲🎲"
+
 
 index_start = read.find(tag_quote_start)
 index_end = read.find(tag_quote_end)
