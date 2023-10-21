9cf578ae023a1af21263b6d78f6b1d30c6339307
MrShadowsWolf <loup.thevenin.1@gmail.com>
2023-10-21 11:43:32 +0200

    tag html + bot + icon

--- README.md
+++ README.md
@@ -1,12 +1,45 @@
 ### Hi there
-
-<!-- INSERT QUOTE START --> 
-
-" On nous a envoyé sauver la Comté Sam, et elle a été sauvée. Mais, pas pour moi… "
-
-_Le Seigneur des anneaux : le retour du roi, Sam_
-
-<!-- INSERT QUOTE END -->
-
-## snake eating my contribution graph
-![snake gif](https://github.com/Loupthevenin/Loupthevenin/blob/output/github-contribution-grid-snake-dark.svg)
+<hr/>
+<h2 align="center">
+ Languages-Frameworks
+</h2>
+<br/>
+<div align="center">
+ <a href="https://skillicons.dev">
+  <img src="https://skillicons.dev/icons?i=python,django,html,css,bootstrap,javascript,github"/>
+ </a>
+</div>
+<hr/>
+<div>
+ <h2>
+  Daily Quote
+ </h2>
+ <br/>
+ <div>
+  <p id="quote">
+   On nous a envoyé sauver la Comté Sam, et elle a été sauvée. Mais, pas pour moi…
+  </p>
+ </div>
+ <br>
+  <div align="right">
+   <p id="movie" style="text-align: right; font-style: italic;">
+    Le Seigneur des anneaux : le retour du roi, Sam
+   </p>
+  </div>
+  <div>
+   <h3>
+    Bot
+   </h3>
+   <p id="bot">
+    🤖 WolfyBOT 🤖 vous informe que ce film a été tiré <b>en cours</b> fois au sort parmis les <b>en cours</b> films sur <b>en cours</b> jours 🎲🎲🎲
+   </p>
+  </div>
+ <br>
+</div>
+<hr/>
+<div>
+ <h2>
+  Contributions
+ </h2>
+ <img alt="snake gif" src="https://github.com/Loupthevenin/Loupthevenin/blob/output/github-contribution-grid-snake-dark.svg"/>
+</div>
--- main.py
+++ main.py
@@ -1,6 +1,8 @@
+from bs4 import BeautifulSoup
+import html
 import json
-import os
 import random
+
 
 path_json = "data_quote.json"
 path_history = "data_history.json"
@@ -8,8 +10,7 @@ path_readme = "README.md"
 base_history = {"history_movie_choices": [],
                 "history_quote_choices": []
                 }
-tag_quote_start = "<!-- INSERT QUOTE START -->"
-tag_quote_end = "<!-- INSERT QUOTE END -->"
+
 
 # load Database
 with open(path_json, "r", encoding="utf-8") as f:
@@ -18,6 +19,7 @@ with open(path_json, "r", encoding="utf-8") as f:
 # load historique
 with open(path_history, "r", encoding="utf-8") as f:
     history = json.load(f)
+
 
 # On tire au sort parmi les films dans un premier temps
 ids_movie = list(set(db["ids"]))
@@ -34,7 +36,7 @@ random_character = db["character"][random_quote_choice]
 
 
 if "\n" in random_quote:
-    random_quote = random_quote.replace("\n", "\n\n")
+    random_quote = random_quote.replace("\n", "<br>")
 
 
 # verifie que le json a le meme modèle que le script
@@ -53,21 +55,26 @@ with open(path_readme, "r") as f:
     read = f.read()
 
 
-# Pour le bot plus tard
-try:
-    chosen = len(history["history_movie_choices"][id_movie])
-except IndexError:
-    chosen = 0
+soup = BeautifulSoup(read, 'html.parser')
+
+# Pour le bot
+chosen = history["history_movie_choices"].count(id_movie)
+days = len(history["history_movie_choices"])
 total_movies = len(ids_movie)
-WolfyBOT = f"🤖 WolfyBOT 🤖 vous informe que ce film a été tiré **{chosen}** fois au sort parmis les **{total_movies}**. 🎲🎲🎲"
+WolfyBOT = f"🤖 WolfyBOT 🤖 vous informe que ce film a été tiré <b>{chosen}</b> fois au sort parmis les <b>{total_movies}</b> films sur <b>{days}</b> jours consécutifs. 🎲🎲🎲"
 
 
-index_start = read.find(tag_quote_start)
-index_end = read.find(tag_quote_end)
+tag_quote = soup.find(id="quote")
+tag_movie = soup.find(id="movie")
+tag_bot = soup.find(id="bot")
 
-if index_start != -1 and index_end != -1:
-    read_update = (f"{read[:index_start + len(tag_quote_start)]} \n\n\" {random_quote} \"\n\n_{random_character}_\n\n"
-                   f"{read[index_end:]}")
+tag_quote.string = random_quote
+tag_movie.string = random_character
+tag_bot.string = WolfyBOT
 
-    with open(path_readme, "w", encoding="utf-8") as f:
-        f.write(read_update)
+# Déséchapper les entités HTML
+read_update = html.unescape(soup.prettify())
+
+# SAVE README
+with open(path_readme, "w", encoding="utf-8") as f:
+    f.write(read_update)
